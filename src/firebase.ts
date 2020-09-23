9809a54e7043ce474881d9833bc4bb863ecef6e9
Joshua Lucas <joshuamlucas91@gmail.com>
2020-09-23 19:18:24 -0400

    configured firebase

---
+++ src/firebase.ts
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const config = {
+  apiKey: 'AIzaSyCE3IOAP1XrO7bfiI3ckVSOJn0smxYdrCg',
+  authDomain: 'joshualucas-porfolio.firebaseapp.com',
+  databaseURL: 'https://joshualucas-porfolio.firebaseio.com',
+  projectId: 'joshualucas-porfolio',
+  storageBucket: 'joshualucas-porfolio.appspot.com',
+  messagingSenderId: '2307884463',
+  appId: '1:2307884463:web:569bf0d1178e020a5f9b5a',
+  measurementId: 'G-QFZ32LNGMG',
+}
+
+// Initialize Firebase
+if (typeof window !== 'undefined' && !firebase.apps.length) {
+  firebase.initializeApp(config)
+  // To enable analytics. https://firebase.google.com/docs/analytics/get-started
+  // if ('measurementId' in config) firebase.analytics()
+}
+
+export const firestore = firebase.firestore()
+
+export default firebase
