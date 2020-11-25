1505d7de439b9594eb3962f3ae6c7fbac133ba22
Joshua Lucas <joshuamlucas91@gmail.com>
2020-11-25 18:59:17 -0500

    create interface for component

--- src/components/Main/about/SkillsComponent.tsx
+++ src/components/Main/about/SkillsComponent.tsx
@@ -6,21 +6,32 @@ const Title = styled.h1``
 
 const SubTitle = styled.h2``
 //Interfaces
-interface ISkillComponent {
+export interface ISkillComponent {
   title: string
-  subtitle: string
+  description: string
+  lang: string[]
+  tools: string[]
 }
 //React Component
-const SkillsComponent: React.FC<ISkillComponent> = ({ title, subtitle }) => {
+const SkillsComponent: React.FC<ISkillComponent> = ({
+  title,
+  description,
+  lang,
+  tools,
+}) => {
   return (
     <>
       <div>
         {/* PUT SVG*/}
         <div>
           <Title>{title}</Title>
-          <SubTitle>{subtitle}</SubTitle>
+          <SubTitle>{description}</SubTitle>
 
           {/* add filp or dropdown section of what skills i have. (i.e. Languages/frame works ) */}
+        </div>
+        <div>
+          <h4>{lang}</h4>
+          <h4>{tools}</h4>
         </div>
       </div>
     </>
