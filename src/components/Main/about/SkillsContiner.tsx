5a889115a67f0a631e4b7aa6529bb806338dd5bc
Joshua Lucas <joshuamlucas91@gmail.com>
2020-11-25 18:58:52 -0500

    moved data fetching logic into lower level to avoid prop drilling

--- src/components/Main/about/SkillsContiner.tsx
+++ src/components/Main/about/SkillsContiner.tsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { db } from '../../../firebase'
+// import SkillsComponent, {ISkillComponent} from './SkillsComponent'
 
 //Styled Components
 const SkillsWrapper = styled.div``
 //Interfaces
-interface ISkillsContinerProps {
-  title: string
-}
 
 //React Component
-const SkillsContiner: React.FC<ISkillsContinerProps> = () => {
+const SkillsContiner: React.FC = () => {
+  const [skillsData, setSkillsData] = useState([])
+  useEffect(function onMount() {
+    async function getPosts() {
+      var snapshot = await db.collection('skills').get()
+
+      var posts = snapshot.docs.map(function getDocs(doc) {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        }
+      })
+      setSkillsData(posts)
+    }
+
+    getPosts()
+  }, [])
+
   return (
     <>
-      <SkillsWrapper></SkillsWrapper>
+      <SkillsWrapper>
+        {console.log(skillsData)}{' '}
+        {/* Change from console log to skills container */}
+      </SkillsWrapper>
     </>
   )
 }
