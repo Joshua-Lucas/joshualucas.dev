3e0ddb4af46e48ad0b3f6c1910856cc2e2ba1688
Joshua Lucas <joshuamlucas91@gmail.com>
2020-11-28 19:15:42 -0500

    adjust the max width

--- src/components/Header/navigation/navigation.tsx
+++ src/components/Header/navigation/navigation.tsx
@@ -6,7 +6,7 @@ import Nav from './nav'
 //Styled Components
 const ComponentContainer = styled.header`
   @media (min-width: 786px) {
-    max-width: 70%;
+    max-width: 75%;
     margin: auto;
     background-color: ${({ theme }) => theme.colors.secondary};
     display: flex;
