dd225e71b0695adfb4d98b18f883c185cc75fe61
Joshua Lucas <joshuamlucas91@gmail.com>
2020-12-18 22:24:34 -0500

    feat(db): establish connection with db and add evn variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,30 @@
 import Header from '../src/components/Header/Header'
 import Main from '../src/components/Main/Main'
+import {connectToDatabase} from './api/db/connect'
 
-export default function Home() {
+export default function Home({isConnected}) {
   return (
     <>
       <Header />
       <Main />
+      {isConnected ? (
+          <h2 className="subtitle">You are connected to MongoDB</h2>
+        ) : (
+          <h2 className="subtitle">
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
     </>
   )
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
+  const { client } = await connectToDatabase()
+
+  const isConnected = await client.isConnected() // Returns true or false
+
   return {
-    props: {},
+    props: { isConnected },
   }
 }
