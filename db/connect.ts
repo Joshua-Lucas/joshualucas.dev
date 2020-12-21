8146b9472451ec14a6a8591129d10a50dd1a162e
Joshua Lucas <joshuamlucas91@gmail.com>
2020-12-20 23:35:58 -0500

    fix(db): add type to db variable

--- db/connect.ts
+++ db/connect.ts
@@ -1,30 +1,22 @@
-import { MongoClient} from 'mongodb'
-
-
-
+import { Db, MongoClient } from 'mongodb'
 
 global.mongo = global.mongo || {}
 
 export async function connectToDB() {
-  if(!global.mongo) {
+  if (!global.mongo.client) {
     const client = new MongoClient(process.env.DATABASE_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       bufferMaxEntries: 0,
       connectTimeoutMS: 10000,
     })
-    
+
     global.mongo.client = client
 
     console.log('connecting to DB')
     await global.mongo.client.connect()
     console.log('conneted to DB')
-
-
   }
-  const db = global.mongo.client.db(process.env.DATABASE_NAME)
-  return {db, dbClient: global.mongo.client}
+  const db: Db = global.mongo.client.db(process.env.DATABASE_NAME)
+  return { db, dbClient: global.mongo.client }
 }
-
-
-
